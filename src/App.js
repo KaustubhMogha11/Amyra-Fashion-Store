2e68871073dc46d6db6d00b16bbb70e8a1a02f53
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-04-21 23:41:47 -0700

    added navbar and sidesearchbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import SideSearch from './components/searchSidebar/SideSearch';
 
 function App() {
+  const [isSideSearch,setIsSideSearch] = useState(false)
   return (
-  
-      <>
+    <>
+      
+    <Navbar sideSearchView={(isSideSearch) => setIsSideSearch(isSideSearch)}/>
+    {isSideSearch?<SideSearch sideSearchView={(isSideSearch) => setIsSideSearch(isSideSearch)}/>:null}
 
-<h1>hiiiiiiiii</h1>
-
-</>
+    </>
   );
 }
 
