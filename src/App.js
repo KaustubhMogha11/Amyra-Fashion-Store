b8abf2f2dd3583de5d511eafcab453b0dc1e4adc
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-04-25 23:05:29 -0700

    added register page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import "./App.css";
 
 import Homepage from "./components/Homepage";
 import Navbar from "./components/navbar/Navbar";
-// import ProductListing from "./components/productListing/ProductListing";
-// import SingleProductPage from "./components/singleProductPage/SingleProductPage";
+import ProductListing from "./components/productListing/ProductListing";
+import SingleProductPage from "./components/singleProductPage/SingleProductPage";
 import Register from "./components/log-reg/Register";
+import Login from "./components/log-reg/Login";
 function App() {
  
   return (
     <>
-    <div style={{height:'auto',width:'auto',overflow:'hidden'}}>
-      {/* <Navbar /> */}
-      <div className="main-cont">
-      {/* <Homepage/> */}
-      {/* <ProductiListing/> */}
-      {/* <ProductiListing/> */}
-      {/* <SingleProductPage/> */}
-      {/* <Login/> */}
-      {/* <Register/> */}
-      </div>
+    <div style={{ height: 'auto', width: 'auto', overflow: 'hidden', position: 'relative' }}>
+        <div className="main-cont">
+          {/* <Homepage/> */}
+          {/* <ProductiListing /> */}
+          {/* <SingleProductPage/> */}
+          {/* <Login/> */}
+          <Register/>
+        </div>
       </div>
     </>
   );
