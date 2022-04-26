82d0c063d28654ccf155c28264627f0729f1b98b
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-04-25 22:51:29 -0700

    added login page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import "./App.css";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/navbar/Navbar";
 // import ProductListing from "./components/productListing/ProductListing";
-import SingleProductPage from "./components/singleProductPage/SingleProductPage";
+// import SingleProductPage from "./components/singleProductPage/SingleProductPage";
+import Register from "./components/log-reg/Register";
 function App() {
  
   return (
     <>
-   {/* <Navbar /> */}
+    <div style={{height:'auto',width:'auto',overflow:'hidden'}}>
+      {/* <Navbar /> */}
       <div className="main-cont">
       {/* <Homepage/> */}
-
-      {/* <ProductListing/> */}
-      <SingleProductPage/>
+      {/* <ProductiListing/> */}
+      {/* <ProductiListing/> */}
+      {/* <SingleProductPage/> */}
+      {/* <Login/> */}
+      {/* <Register/> */}
+      </div>
       </div>
     </>
   );
