80d100cc71e057953ff99111ee8d56c2d1f2ac23
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-01 19:01:05 -0700

    remove errors

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -13,6 +13,5 @@ router.post('/login', userLogIn);
 
 router.get('/products', getProducts);
 router.get('/product/:id', getProductById);
-router.post('/payment', addPaymentGateway);
-router.post('/callback', paymentResponse);
+
 export default router;
