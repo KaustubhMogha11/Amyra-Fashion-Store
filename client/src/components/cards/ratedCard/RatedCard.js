2e59a6e8b11e004db75f8845b41bb3974e865890
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-01 18:56:50 -0700

    fixed some ui issues

--- client/src/components/cards/ratedCard/RatedCard.js
+++ client/src/components/cards/ratedCard/RatedCard.js
@@ -3,22 +3,25 @@ import "./ratedcard.css"
 import StarRating from './StarRate'
 import {Link} from 'react-router-dom'
 const RatedCard = (props) => {
+  const isoutOfStock = props.stock
   return (
     <>
     <Link to='/product-page' className='rated-card-cont link'>
         <div className="rated-card-cont">
-            <div className="rated-card-img">
-            <img src={props.imgSrc} alt="card" />
+        <div id="rated-card-img">
+            <img src={props.imgSrcTwo} alt="card" id='cardHoverImg' />
+                <img src={props.imgSrcOne} alt="card" />
+                {isoutOfStock?<div className="outOfStockLabel">Out of stock</div>:null}
             </div>
             <div className="rated-card-content">
                 <StarRating/>
-                <div className="card-content-type">Clothing</div>
+                <div className="card-content-type">{props.type}</div>
                 <div className="card-content-name-like">
                 <div className="card-content-name">{props.name}</div>
                     <div className="card-like"> <img src="images/like.png" alt="like" /></div>
                 </div>
                 <div className="card-animated-btn">
-                ₹ 1000.00
+               {props.price}
                 </div>
             </div>
         </div>
