7bae582f60c959bf1e3f0820fb2170a387c67639
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-03 18:03:32 -0700

    made best selling section dynamic

--- client/src/components/bestSelling/BestSelling.js
+++ client/src/components/bestSelling/BestSelling.js
@@ -1,29 +1,30 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import "./bestselling.css";
 import ColorCard from "../cards/ColorChooseCard/ColorCard"
 import PriceCard from "../cards/priceCard/PriceCard"
+import { getProducts as listProducts } from '../../redux/actions/productActions';
+import { useSelector, useDispatch } from 'react-redux'; // hooks
 const BestSelling = () => {
+  const {products}=useSelector(state => state.getProducts);
+    const dispatch = useDispatch();
+    
+
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [dispatch])
   return (
     <>
       <div className="best-selling-sec">
         <div className="bestselling-title">BEST SELLING PRODUCTS</div>
         <div className="scrolling-cont">
         <div className="best-selling-card-cont">
-        <ColorCard />
-          <PriceCard
-            imgSrc={"images/card/women-1.jpeg"}
-            name={"MISSGUIDED RIBBED WRAP FRONT SLEEVELESS CROP TOP"}
-          />
-
-          <PriceCard
-            imgSrc={"images/card/women-3.jpeg"}
-            name={"LAVISH ALICE DEEP BANDEAU ASYMMETRIC HEM MIDI DRESS"}
-          />
-          <PriceCard
-            imgSrc={"images/card/shirt-2.jpeg"}
-            name={"SELECTED HOME OVERCOAT"}
-          />
-          <ColorCard />
+        {products.slice(4,9).map((val,index)=>{
+                return(
+                    <div key={index}>
+                    <PriceCard ifD={val.id} imgSrcTwo={val.imgTwo} imgSrcOne={val.imgOne} stock={val.stock} name={val.title} type={val.type} price={val.price}/>
+                    </div>
+                )
+            })}
         </div>
         </div>
 
