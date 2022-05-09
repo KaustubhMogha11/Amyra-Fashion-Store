c7b534a69c578085a59e3c41a10932f707b0b453
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-05-08 18:11:34 -0700

    added paytm controller

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import DefaultData from './default.js';
 import Routes from './routes/routes.js';
-
+import { v4 as uuid } from 'uuid';
 dotenv.config();
 
 const username = process.env.DB_USERNAME;
@@ -36,3 +36,16 @@ app.listen(port, function (err) {
 // default data to database
 DefaultData();
 
+
+export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams = {};
+paytmParams['MID'] = process.env.PAYTM_MID,
+paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
+paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
+paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
+paytmParams['ORDER_ID'] = uuid(),
+paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
+paytmParams['TXN_AMOUNT'] = '100',
+paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
+paytmParams['EMAIL'] = 'kaustubhmogha10@gmail.com'
+paytmParams['MOBILE_NO'] = '1234567852'
